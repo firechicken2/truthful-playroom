6f2b9f10a4e878cb7b91e3196c8c15d933010094
firechicken2 <99267256+firechicken2@users.noreply.github.com>
2022-02-12 23:35:45 +0800

    Add files via upload
    //score get
    let rareScore = 1;
    let epicScore = 6;
    let legendScore = 10;
    
    //range 1~4
    let socreHigh = 1.5;
    let socreLow = 2.4;
    
    //combo
    comboValue += scoreGet/10;
    scoreCount = scoreCount * comboValue;

--- sketch_0210.js
+++ sketch_0210.js
@@ -1,5 +1,16 @@
+//score get
+let gooudScore = 0.1;
+let rareScore = 1;
+let epicScore = 6;
+let legendScore = 10;
+
+//range 1~4
+let socreHigh = 1.5;
+let socreLow = 2.4;
+
 let shoot = false;
 let reStart = false;
+let arrowCount = 0;
 
 class arrow {
   constructor(name, pos_x, pos_y) {
@@ -119,7 +130,6 @@ class target {
     this.yMax = ymax;
     this.yTarget = this.yMax;   
     this.yBest = (this.yMin + this.yMax)/2;
-    console.log(this.yBest);
   }
 }
 
@@ -139,12 +149,16 @@ function setup() {
     testTarget.yRangeSet(height*1/4, height*3/4);
 
     audio_bonk = createAudio('bonk.mp3');
+    rareColor = color(65,105,225);
+    epicColor = color(187,41,187);
+    legendColor = color(255,163,0);
 }
 
-function draw() {
+function draw() { 
   if(!shoot){
     background(240);
   }else{
+    rectMode(CORNER);
     fill(240,10);
     noStroke();
     rect(0,0,width,height);
@@ -155,7 +169,6 @@ function draw() {
   judge();
   scoreDisplay();
   youAreGooud();
-  talkShit();
 }
 
 function mouseReleased() { 
@@ -170,7 +183,7 @@ function mousePressed() {
 }
 
 function youAreGooud(){
-  if(scoreCount > 0 && reStart){
+  if(reStart){
     textStyle(BOLD);
     noStroke();
     textSize(72);
@@ -179,35 +192,7 @@ function youAreGooud(){
   }
 }
 
-let hit = 0;
-let talkShitTime = 0;
-
-function talkShit(){
-  if(talkShitTime > 0){
-    textStyle(BOLD);
-    noStroke();
-    textSize(72);
-    if(hit >= 1.2){
-      fill(	0, 0, 128);
-      text("稀   油", width/2,250);
-    }else if( 0.6 < hit && hit < 1.2){
-      fill(	128, 0, 128);
-      text("史私", width/2,250);
-    }else if(hit <= 0.6){
-      fill(	218, 165, 32);
-      text("哇 窩 船 說", width/2,250);
-    }
-    talkShitTime -= 1;
-  }
-}
-
 function judge(){
-  // out of canvas size
-  if(testArrow.absX < 0 || testArrow.absX > 390 || testArrow.absY > 390 || testArrow.absY < 0){
-    testArrow.reset();
-    reStart = true;
-  }
-  
   let d = dist(testArrow.absX,testArrow.absY,testTarget.x,testTarget.y);
   
   if(d < testTarget.size/2){
@@ -217,25 +202,108 @@ function judge(){
     let _dm = abs(testTarget.y - _m);
     let _am = abs(testTarget.yMax - _m);
     let _lv = _dm / _am;
-    hit = map(_lv,0,1,1,4);
+    hit = nfc(map(_lv,0,1,1,4),3);
+    let _hitLevel = hitLevelCount(hit);
     talkShitTime = 100;
-    console.log(hit);
+    console.log(_hitLevel);
     //
+    comboHitCheck(_hitLevel);
     testTarget.reset();
     testArrow.reset();
-    scoreAdd(10);
+    scoreAdd(10*_hitLevel);
+    scoreRecord(hit);
+    scoreCount = scoreCount * comboValue;
+  }
+
+  // out of canvas size
+  if(testArrow.absX < 0 || testArrow.absX > 390 || testArrow.absY > 390 || testArrow.absY < 0){
+    testArrow.reset();
+    scoreRecord(-1.0); 
+  }
+
+  talkShit();
+}
+
+let hit = 0;
+let talkShitTime = 0;
+
+function hitLevelCount(_hit){
+  let _result =1;
+  if(_hit >= socreLow){
+    _result = rareScore;
+  }else if( socreHigh < _hit && _hit < socreLow){
+    _result = epicScore;
+  }else if(0 <_hit && _hit <= socreHigh){
+    _result = legendScore;
+  }else if(_hit == -1){
+    //
+    _result = -1;
+  }else if(_hit == 0){
+    //
+    _result = 0;
+  }
+  return _result;
+}
+
+let rareColor;
+let epicColor;
+let legendColor;
+
+function talkShit(){
+  if(talkShitTime > 0){
+    textStyle(BOLD);
+    noStroke();
+
+    let _hitLevel = hitLevelCount(scores[arrowCount-1]);
+
+    if(_hitLevel == rareScore ){
+      fill(	rareColor );
+      textSize(72);
+      text("稀   油", width/2,250);
+      textSize(24);
+      text(hit, width/2,250 + 24*2);
+    }else if( _hitLevel == epicScore ){
+      fill(	epicColor );
+      textSize(72);
+      text("史私", width/2,250);
+      textSize(24);
+      text(hit, width/2,250 + 24*2);
+    }else if(_hitLevel == legendScore ){
+      fill(	legendColor );
+      textSize(72);
+      text("哇 窩 船 說", width/2,250);
+      textSize(24);
+      text(hit, width/2,250 + 24*2);
+    }
+    talkShitTime -= 1;
+  }
+}
+
+let comboValue = 1;
+function comboHitCheck( _hitLevel){
+  let _pastHitLevel = hitLevelCount(scores[arrowCount-1]);
+  if(_hitLevel == _pastHitLevel){
+    comboValue += _hitLevel/10;
+    console.log("combo");
+  }else{
+    comboValue = 1;
   }
 }
 
 let scoreCount = 0;
 
 function scoreAdd(_add){
-  scoreCount += 10;
+  scoreCount += _add;
 }
 
 function scoreReset(){
   scoreCount = 0;
+  scores = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0];
+  arrowCount = 0;
 }
+
+let scoreSize = 21;
+let rectBtw = 36;
 
 function scoreDisplay(){
   textStyle(NORMAL);
@@ -243,11 +311,41 @@ function scoreDisplay(){
   fill(0);
   textSize(24);
   text(scoreCount, width/2,50);
+
+  for(let i =0;i<scores.length;i++){
+    let _hitLevel = hitLevelCount(scores[i]);
+    if(_hitLevel == rareScore){
+      fill(	rareColor );
+    }else if(_hitLevel == epicScore){
+      fill(	epicColor );
+    }else if(_hitLevel == legendScore){
+      fill(	legendColor );
+    }else if(_hitLevel == -1){
+      fill(	210 );
+    }else if(_hitLevel == 0){
+      fill(	255 );
+    }
+
+    ellipse(i*rectBtw + width/2 - (scores.length-1)*rectBtw/2,height-scoreSize*1.2,scoreSize,scoreSize);
+  }
 }
 
 function fontSet(){
   textAlign(CENTER);
   textSize(24);
   textFont('Helvetica');
+}
+
+let scores = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0];
+
+function scoreRecord( _hit ){
+  scores[arrowCount] = _hit
+
+  if(arrowCount <9){
+    arrowCount += 1;
+  }else{
+    reStart = true; 
+  }
+  
 }
 
